bd8fb662206896f0cbe2b002701180e2eaf946ac
developersung13 <developersung13@gmail.com>
2023-11-25 22:14:11 +0900

    [feat]: '교수정보' 페이지 상단의 대표 아이콘 변경 (#30)

--- app/prof-infos/page.tsx
+++ app/prof-infos/page.tsx
@@ -5,7 +5,14 @@ export default function ProfInfos() {
   return (
     <div className='mt-2 px-5 2lg:px-0 overflow-x-auto'>
       <div className='flex flex-col w-[60rem] mx-auto'>
-        <p className='text-2xl font-semibold'>👨🏻‍🏫 교수 정보</p>
+        <p className='flex items-center gap-3'>
+          <img
+            src='/images/icons/person_flag.png'
+            alt='logo'
+            style={{ width: '47.5px' }}
+          />
+          <span className='text-2xl font-semibold'>교수 정보</span>
+        </p>
         <div className='mt-10 mb-10 grid grid-cols-2 gap-7'>
           <ProfInfo />
           <ProfInfo />
